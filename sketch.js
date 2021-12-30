5179cf84f2ac649958c7ffe74cd2ac1227d3ab10
Ivonne Sandoval <84480065+Ivonneisr@users.noreply.github.com>
2021-12-30 10:14:14 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
+
+function preload() {
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  
+}
+
+function setup() {
+  createCanvas(600, 200);
+
+  //crear sprite de trex 
+  
+  
+  //crear sprite de suelo
+  
+
+  //crear sprite de suelo invisible
+ 
+  
+}
+
+function draw() {
+  background(220);
+
+
+  //hacer que el trex salte al presionar la barra espaciadora
+  
+
+  drawSprites();
+}
